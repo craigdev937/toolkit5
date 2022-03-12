a1ca9e91d406853324b110fe81d7d4dc28d96d5f
Craig <indigomx9@gmail.com>
2022-03-11 20:02:09 -0500

    Added the GetOne, Update, and Delete methods.

--- src/controllers/contCon.js
+++ src/controllers/contCon.js
@@ -25,6 +25,43 @@ class Contact {
             next(error);
         }
     };
+
+    GetOne = async (req, res, next) => {
+        try {
+            await Contacts.findById(req.params.id)
+            .then((contact) => res.status(200).json(contact));
+        } catch (error) {
+            res.status(500).json({msg: error.message});
+            next(error);
+        }
+    };
+
+    Update = async (req, res, next) => {
+        try {
+            await Contacts.findByIdAndUpdate(req.params.id)
+                .then((contact) => {
+                    contact.name = req.body.name,
+                    contact.email = req.body.email,
+                    contact.phone = req.body.phone,
+                    contact.save()
+                    .then(() => res.json(contact));
+                })
+        } catch (error) {
+            res.status(500).json({msg: error.message});
+            next(error);
+        }
+    };
+
+    Delete = async (req, res, next) => {
+        try {
+            await Contacts.findByIdAndDelete(req.params.id)
+            .then(() => res.status(200)
+            .json("The Contact was Deleted!"));
+        } catch (error) {
+            res.status(500).json({msg: error.message});
+            next(error);
+        }
+    };
 };
 
 export const CONTACT = new Contact;
--- src/routes/contRt.js
+++ src/routes/contRt.js
@@ -4,6 +4,9 @@ import { CONTACT } from "../controllers/contCon.js";
 export const contRt = express.Router();
     contRt.post("/", CONTACT.Create);
     contRt.get("/", CONTACT.FetchAll);
+    contRt.get("/:id", CONTACT.GetOne);
+    contRt.put("/:id", CONTACT.Update);
+    contRt.delete("/:id", CONTACT.Delete);
 
 
 
